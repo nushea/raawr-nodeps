6f43340b72627256e87e6ccb45aa9f35b0f4e422
Shea Duma <nu@she-a.eu>
2025-10-06 05:19:14 +0300

    added base traversal

--- js/folderLogic.js
+++ js/folderLogic.js
@@ -1,12 +1,61 @@
 const div = document.currentScript.parentElement;
+var curpath = "/";
+var pathHistory = [];
+
+function setCookie(name, value) {
+    document.cookie = name + "=" + encodeURIComponent(value) + "; path=/";
+}
+
+function getCookie(name) {
+    const nameEQ = name + "=";
+    const cookies = document.cookie.split(';');
+    for (let i = 0; i < cookies.length; i++) {
+        let c = cookies[i].trim();
+        if (c.indexOf(nameEQ) === 0) return decodeURIComponent(c.substring(nameEQ.length));
+    }
+    return null;
+}
+function deleteCookie(name) {
+    setCookie(name, "");
+}
+
+async function filList(pathname) {
+  const curPath = pathname;
+
+  try {
+    const res = await fetch('http://localhost/api/wawAPI/' + curPath)
+    const text = await res.text();
+	if((curPath.match(/\//g) || []).length == 2 && curPath.substring(1, curPath.lastIndexOf('/')) == "home")
+		setCookie('Username', curPath.substring(curPath.lastIndexOf('/')+1));
+    return text.split('\n');
+  } catch (err) {
+    console.error('fetch failed:', err)
+    return 'error fetching'
+  }
+}
 
 function FileBlock(path, type, name){
-	if(type == "folder")
+	if(type == "#")
 		typePath = "/img/icons8-folder-96.png";
 	else
 		typePath = "/img/icons8-file-96.png";
-	return "<a href=\""+ path +"\" class=\"card\"><img class=\"logo\" src=\""+typePath+"\"><p class>"+name+"</p></a>";
+	return '<a href="#" onClick="clickedFolder(\''+path+ '\'); return false;" class="card"><img class="logo" src="'+typePath+'"><p class>'+name+'</p></a>';
 }
-div.innerHTML = FileBlock("fire", "file", "outerwildes");
-for(let i=0;i<30;i++)
-	div.innerHTML += FileBlock("fire", "file", "outerwildes");
+function getFiles(pathname){
+	pathname = pathname.trim();
+	curpath = pathname;
+	var files;
+	filList(pathname).then(result => {
+		files = result;
+		var FileTable = "";
+		files.forEach((file) => { if(file.trim().length > 0) FileTable += FileBlock(file.substring(file.lastIndexOf(" ")), file.substring(0,1), file.substring(file.lastIndexOf("/")+1) )});
+		div.innerHTML = FileTable;
+	});
+}
+function clickedFolder(pathname){
+	pathHistory.push(pathname);
+	console.log(pathHistory);
+	getFiles(pathname);
+}
+getFiles("/");
+
